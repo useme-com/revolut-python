e480d37e7d0b3bbfd158f9421765e30d15c7722e
Michał Sałaban <michal@salaban.info>
2021-09-23 14:06:47 +0200

    Bump up version

--- revolut/__init__.py
+++ revolut/__init__.py
@@ -13,7 +13,7 @@ except ImportError:  # pragma: nocover
     from urllib import urlencode
 from . import exceptions, utils
 
-__version__ = "0.8"
+__version__ = "0.8.1"
 
 _log = logging.getLogger(__name__)
 
