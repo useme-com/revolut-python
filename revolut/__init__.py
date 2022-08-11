19194933c968047087d5f0f4407442e95a1b1bdf
Rafal Fuchs <r.fuchs@useme.eu>
2022-08-11 10:14:56 +0200

    convert value

--- revolut/__init__.py
+++ revolut/__init__.py
@@ -621,5 +621,5 @@ class Order(_UpdateFromKwargsMixin):
         self.updated_at = (
             dateutil.parser.parse(self.updated_at) if self.updated_at else None
         )
-        self.value = Decimal(self.order_amount["value"]/100) if self.order_amount else ""
+        self.value = "{:.2f}".format(Decimal(self.order_amount["value"]/100)) if self.order_amount else ""
         self.currency = self.order_amount["currency"] if self.order_amount else ""
