2a8c3ce315210b8019dd8e26e4cd3468259c8250
Rafal Fuchs <r.fuchs@useme.eu>
2022-08-19 14:43:43 +0200

    make _datetime pretty again

--- revolut/utils.py
+++ revolut/utils.py
@@ -27,12 +27,21 @@ def _date(v):
         return v.date()
     return v
 
+
 def _datetime(v):
     if not isinstance(v, (datetime.date, datetime.datetime)):
-        return dateutil.parser.parse(v).date().strftime('%Y-%m-%dT%H:%M:%S.%f%zZ')
-    elif isinstance(v, datetime.datetime):
-        return v.date().strftime('%Y-%m-%dT%H:%M:%S.%f%zZ')
+        v = datetime.date.fromisoformat(v)
     return v.strftime('%Y-%m-%dT%H:%M:%S.%f%zZ')
+
+
+def _datetime(v):
+    def _format(date):
+        return date.strftime('%Y-%m-%dT%H:%M:%S.%f%zZ')
+    if not isinstance(v, (datetime.date, datetime.datetime)):
+        date = dateutil.parser.parse(v).date()
+    elif isinstance(v, datetime.datetime):
+        date = v.date()
+    return _format(date) if date else _format(v)
 
 def _integertomoney(value_int):
     return (Decimal(value_int) / Decimal(100)).quantize(Decimal("0.01"))
