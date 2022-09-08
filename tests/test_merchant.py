d32efe0ebcf613c37eb119cc21f9a678582408e3
Michał Sałaban <michal@salaban.info>
2022-09-08 15:11:51 +0200

    Test for URL and data basic structure

--- tests/test_merchant.py
+++ tests/test_merchant.py
@@ -17,14 +17,25 @@ class TestRevolutMerchant(TestCase, JSONResponsesMixin):
     @responses.activate
     def test_orders(self):
         ORDER_ID = "0f1e2ffc-6cd4-45be-8fb6-da3705cf321f"
+        ORDER_REFERENCE = "a test order in PLN"
         responses.add(
             responses.POST,
             "https://sandbox-merchant.revolut.com/api/1.0/orders",
             json=self._read("10-create_order.json"),
             status=200,
+            match=[
+                responses.matchers.query_string_matcher(""),
+                responses.matchers.json_params_matcher(
+                    {
+                        "amount": 1234,
+                        "currency": "PLN",
+                        "merchant_order_ext_ref": ORDER_REFERENCE,
+                    }
+                ),
+            ],
         )
         cli = MerchantClient(self.merchant_key, sandbox=True)
-        order = cli.create_order(Decimal("12.34"), "PLN", "a test order in PLN")
+        order = cli.create_order(Decimal("12.34"), "PLN", ORDER_REFERENCE)
         self.assertEqual(order.value, Decimal("12.34"))
         self.assertDictEqual(order.order_amount, {"value": 1234, "currency": "PLN"})
         self.assertEqual(order.id, ORDER_ID)
@@ -45,6 +56,7 @@ class TestRevolutMerchant(TestCase, JSONResponsesMixin):
             "https://sandbox-merchant.revolut.com/api/1.0/orders",
             json=self._read("20-orders.json"),
             status=200,
+            match=[responses.matchers.query_string_matcher("")],
         )
         orders = cli.orders()
         self.assertEqual(len(orders), 2)
@@ -56,6 +68,7 @@ class TestRevolutMerchant(TestCase, JSONResponsesMixin):
             f"https://sandbox-merchant.revolut.com/api/1.0/orders/{ORDER_ID}",
             json=self._read("30-order.json"),
             status=200,
+            match=[responses.matchers.query_string_matcher("")],
         )
         order = cli.get_order(ORDER_ID)
         self.assertEqual(order.value, Decimal("12.34"))
